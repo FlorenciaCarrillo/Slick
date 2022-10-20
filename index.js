77f3e00f04ed683283629055b8958004b18cb6ff
Florencia <florencianoelcarrillo@gmail.com>
2022-10-19 21:39:16 -0300

    se agregan componentes al carrousel

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const carrousel = document.getElementById("carrouselAnimales");
 
-$(document).ready(function () {
+fetch("animales.json").then(response => response.json()).then(animales => {
+    animales.forEach(animal => {
+        const divAnimal = document.createElement('div');
+        divAnimal.innerHTML = `
+        <h1> ${animal.nombre} </h1>
+        `;
+        carrousel.appendChild(divAnimal);
+    });
     $('.carrousel').slick({
     });
-});
 
-
+})
